2d9ecff46c0a1024b15d736ecc029ebad0fe4c41
Andres Felipe <nbalamejor@hotmail.com>
2023-07-28 18:35:35 -0500

    Se creo el esquema Admin y se crea la tabla menu

---
+++ WebAppTransporte/DatabaseTransporte/Admin/TMenus.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [Admin].[TMenus]
+(
+	idMenu bigint identity(1,1) not null,
+	idMenuPadre bigint null,
+	nombre varchar(400) not null,	
+	nombreController varchar(400) null,	
+	nombreAction varchar(400) null,
+	esNodo bit,
+	activo bit,
+	primary key (idMenu),
+    FOREIGN KEY (idMenuPadre) REFERENCES Admin.TMenus(idMenu),
+)
