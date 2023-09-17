9574e9588f8d2e3aa92d7c25a5d7cef0cb498f32
jbetancurt <jbetancurt@gmail.com>
2023-09-17 16:25:12 -0500

    Ajuste AutController

--- WebAppTransporte/WebAppTransporte/Controllers/AuthController.cs
+++ WebAppTransporte/WebAppTransporte/Controllers/AuthController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Azure;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.IdentityModel.Tokens.Jwt;
 using System.Net.Http.Headers;
 using System.Text.Json;
+using WebAppTransporte.Models.Usuarios.MSN;
 
 namespace WebAppTransporte.Controllers
 {
@@ -41,6 +45,63 @@ namespace WebAppTransporte.Controllers
 			return Redirect(URL);
 		}
 
+		[HttpPost("")]
+		public async Task<IActionResult> Autenticar([FromBody] loginRequest modelo)
+		{
+			string grant_type = "authorization_code";
+
+			Dictionary<string, string> BodyData = new Dictionary<string, string>()
+			{
+				{ "grant_type", grant_type },
+				{ "code", modelo.code },
+				{ "Redirect_uri", this.RedirectURI },
+				{ "client_id", this.ClientId },
+				{ "client_secret", this.Secret },
+				{ "scope", this.Scope }
+			};
+			HttpClient client = new HttpClient();
+			var body = new FormUrlEncodedContent(BodyData);
+			var response = await client.PostAsync(TokenEndPoint, body);
+			var status = $"{(int)response.StatusCode} {response.ReasonPhrase}";
+
+			var jsonCOntent = await response.Content.ReadFromJsonAsync<JsonElement>();
+
+			var prettyJson = JsonSerializer.Serialize(jsonCOntent, new JsonSerializerOptions { WriteIndented = true });
+
+			var accesToken = JsonDocument.Parse(prettyJson).RootElement.GetProperty("access_token").GetString();
+			var jwtToken = new JwtSecurityToken(accesToken);
+			var ss = jwtToken.Subject;
+			var httpClient = new HttpClient();
+			httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accesToken);
+
+			var response1 = await httpClient.GetAsync(API_EndPoint);
+			if (response1.IsSuccessStatusCode)
+			{
+				var resultado = await response1.Content.ReadAsStringAsync();
+
+				var jsonResult = Newtonsoft.Json.JsonConvert.DeserializeObject(resultado) as Newtonsoft.Json.Linq.JObject;
+				var jsUsuarioMSN = Newtonsoft.Json.JsonConvert.DeserializeObject< UsuarioMSN>(resultado);
+				var jsonRes = JsonSerializer.Deserialize<JsonElement>(resultado);
+				var js2 = JsonSerializer.Deserialize<JsonElement>(jsonRes,
+						new JsonSerializerOptions
+						{
+							WriteIndented = true,
+							Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+
+						}
+					);
+				status = $"{(int)response1.StatusCode} {response1.ReasonPhrase}";
+				return Ok(jsUsuarioMSN);
+				//return $"{status + Environment.NewLine}{js2 + Environment.NewLine}{response1.IsSuccessStatusCode}";
+			}
+			else
+			{
+				return Ok(new UsuarioMSN());
+			}
+		}
+	
+
+
 		[HttpGet("login-callback")] 
 		public async Task<IActionResult> GetCallback([FromQuery] string code, string state) 
 		{
@@ -65,18 +126,29 @@ namespace WebAppTransporte.Controllers
 			var prettyJson = JsonSerializer.Serialize(jsonCOntent, new JsonSerializerOptions { WriteIndented = true });
 
 			var accesToken = JsonDocument.Parse(prettyJson).RootElement.GetProperty("access_token").GetString();
-
+			var jwtToken = new JwtSecurityToken(accesToken);
+			var ss = jwtToken.Subject;
 			var httpClient = new HttpClient();
 			httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",accesToken);
 
 			var response1 = await httpClient.GetAsync(API_EndPoint);
 			if (response1.IsSuccessStatusCode)
 			{
-				var jsonRes = JsonSerializer.Deserialize<JsonElement>(
-						await response1.Content.ReadAsStringAsync()
+				var resultado = await response1.Content.ReadAsStringAsync();
+
+				var jsonResult = Newtonsoft.Json.JsonConvert.DeserializeObject(resultado) as Newtonsoft.Json.Linq.JObject;
+				var js = Newtonsoft.Json.JsonConvert.DeserializeObject(resultado);
+				var jsonRes = JsonSerializer.Deserialize<JsonElement>(resultado);
+				var js2 = JsonSerializer.Deserialize<JsonElement>(jsonRes,
+						new JsonSerializerOptions
+						{
+							WriteIndented = true,
+							Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+
+						}
 					);
 				status = $"{(int)response1.StatusCode} {response1.ReasonPhrase}";
-				return Ok($"{status + Environment.NewLine}{jsonRes + Environment.NewLine}{response1.IsSuccessStatusCode}");
+				return Ok($"{status + Environment.NewLine}{js2 + Environment.NewLine}{response1.IsSuccessStatusCode}");
 			}
 			else
 			{
---
+++ WebAppTransporte/WebAppTransporte/Models/Usuarios/MSN/UsuarioMSN.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAppTransporte.Models.Usuarios.MSN
+{
+	public class UsuarioMSN
+	{
+		public string id { get; set; }
+		public List<string> businessPhones { get; set; } = new List<string>();
+		public string displayName { get; set; }
+		public string givenName { get; set; }
+		public string surname { get; set; }
+	}
+}
